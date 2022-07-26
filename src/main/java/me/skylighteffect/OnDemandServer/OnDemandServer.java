ad5c115982cc027d3588627151b974f45b953456
SkyLightEffect <71511518+SkyLightEffect@users.noreply.github.com>
2022-07-26 08:36:53 +0200

    Fixed plugin startup

--- src/main/java/me/skylighteffect/OnDemandServer/OnDemandServer.java
+++ src/main/java/me/skylighteffect/OnDemandServer/OnDemandServer.java
@@ -1,19 +1,28 @@
 package me.skylighteffect.OnDemandServer;
 
+import jdk.jpackage.internal.Log;
 import me.skylighteffect.OnDemandServer.configs.MainCFG;
 import me.skylighteffect.OnDemandServer.configs.MsgCFG;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public final class OnDemandServer extends Plugin {
 
+    public static Plugin plugin;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
 
+        plugin = this;
+        
+        // Write default configuration folder
+        if (!getDataFolder().exists())
+            getDataFolder().mkdir();
+
         MainCFG.loadConfig(this);
         MsgCFG.loadConfig(this);
 
-        MsgCFG.getContent("plugin_enabled");
+        getLogger().info(MsgCFG.getContent("plugin_enabled"));
     }
 
     @Override
