67b486325921381df46d6f29467ba910e2d324cd
SkyLightEffect <71511518+SkyLightEffect@users.noreply.github.com>
2022-07-26 08:16:18 +0200

    MsgCFG integration

--- src/main/java/me/skylighteffect/OnDemandServer/OnDemandServer.java
+++ src/main/java/me/skylighteffect/OnDemandServer/OnDemandServer.java
@@ -1,5 +1,7 @@
 package me.skylighteffect.OnDemandServer;
 
+import me.skylighteffect.OnDemandServer.configs.MainCFG;
+import me.skylighteffect.OnDemandServer.configs.MsgCFG;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public final class OnDemandServer extends Plugin {
@@ -7,6 +9,10 @@ public final class OnDemandServer extends Plugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        MainCFG.loadConfig(this);
+        MsgCFG.loadConfig(this);
+
+        MsgCFG.getContent("plugin_enabled");
     }
 
     @Override
---
+++ src/main/java/me/skylighteffect/OnDemandServer/configs/MainCFG.java
@@ -0,0 +1,38 @@
+package me.skylighteffect.OnDemandServer.configs;
+
+import me.skylighteffect.OnDemandServer.OnDemandServer;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+public class MainCFG {
+    private static Configuration config;
+    private final static String filename = "config.yml";
+
+    public static void loadConfig(Plugin plugin) {
+
+        // read config
+
+        File file = new File(plugin.getDataFolder(), filename);
+        if (!file.exists()) {
+            try (InputStream in = plugin.getResourceAsStream(filename)) {
+                Files.copy(in, file.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // load config
+        try {
+            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(plugin.getDataFolder(), filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
