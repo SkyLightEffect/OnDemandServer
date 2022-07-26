c0e3b839ed3945bda72b4d0e2f9f22a2deb77f1b
SkyLightEffect <71511518+SkyLightEffect@users.noreply.github.com>
2022-07-26 09:05:47 +0200

    Added ServerConnectListener.java

--- src/main/java/me/skylighteffect/OnDemandServer/OnDemandServer.java
+++ src/main/java/me/skylighteffect/OnDemandServer/OnDemandServer.java
@@ -3,6 +3,7 @@ package me.skylighteffect.OnDemandServer;
 import jdk.jpackage.internal.Log;
 import me.skylighteffect.OnDemandServer.configs.MainCFG;
 import me.skylighteffect.OnDemandServer.configs.MsgCFG;
+import me.skylighteffect.OnDemandServer.listener.ServerConnectListener;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public final class OnDemandServer extends Plugin {
@@ -22,11 +23,16 @@ public final class OnDemandServer extends Plugin {
         MainCFG.loadConfig(this);
         MsgCFG.loadConfig(this);
 
+        // Register events
+        getProxy().getPluginManager().registerListener(this, new ServerConnectListener());
+
+        // Final message
         getLogger().info(MsgCFG.getContent("plugin_enabled", plugin.getDescription().getVersion()));
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info(MsgCFG.getContent("plugin_disabled", plugin.getDescription().getVersion()));
     }
 }
---
+++ src/main/java/me/skylighteffect/OnDemandServer/listener/ServerConnectListener.java
@@ -0,0 +1,12 @@
+package me.skylighteffect.OnDemandServer.listener;
+
+import net.md_5.bungee.api.event.ServerConnectEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+public class ServerConnectListener implements Listener {
+    @EventHandler
+    public void onServerConnect(ServerConnectEvent e) {
+
+    }
+}
