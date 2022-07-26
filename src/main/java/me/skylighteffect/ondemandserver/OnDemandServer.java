795704fede8e7af4cb2a6fad41a174a4a4d8fc37
SkyLightEffect <71511518+SkyLightEffect@users.noreply.github.com>
2022-07-26 07:40:50 +0200

    Initial commit

---
+++ src/main/java/me/skylighteffect/ondemandserver/OnDemandServer.java
@@ -0,0 +1,16 @@
+package me.skylighteffect.ondemandserver;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class OnDemandServer extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
